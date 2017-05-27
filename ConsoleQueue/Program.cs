652fcc2db8da9a7a4b72e944a2867b7374d81e37
OlegDe <olegde@ukr.net>
2017-05-27 07:30:48 +0300

    DZ_3

---
+++ ConsoleQueue/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; // Namespace for Queue storage types
+
+
+
+namespace ConsoleQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CreateQueue();
+
+            AddMessageQueue("Hello, World");
+
+            PeekMessageQueue();
+
+            AddMessageQueue("Hello, Step");
+
+            PeekMessageQueue();
+
+            MessageCountQueue();
+
+        }
+
+        static void CreateQueue()
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("key1"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a container.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist
+            queue.CreateIfNotExists();
+        }
+
+
+        static void AddMessageQueue(string s)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("key1"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist.
+            queue.CreateIfNotExists();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(s);
+            queue.AddMessage(message);
+        }
+
+        static void PeekMessageQueue()
+        {
+            // Retrieve storage account from connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("key1"));
+
+            // Create the queue client
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Peek at the next message
+            CloudQueueMessage peekedMessage = queue.PeekMessage();
+
+            // Display message.
+            Console.WriteLine(peekedMessage.AsString);
+
+        }
+
+        static void MessageCountQueue()
+            {
+            try
+        {// Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("key1"));
+
+        // Create the queue client.
+        CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+        // Retrieve a reference to a queue.
+        CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+        // Fetch the queue attributes.
+        queue.FetchAttributes();
+
+            // Retrieve the cached approximate message count.
+            int? cachedMessageCount = queue.ApproximateMessageCount;
+
+        // Display number of messages.
+        Console.WriteLine("Number of messages in queue: " + cachedMessageCount);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+   
+        }
+
+    }
+}
